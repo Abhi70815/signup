ced08a663231807d79d7326a711cac15657ab568
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-07 13:47:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
+document.querySelector('form').addEventListener('submit', ()=>{
+  
 let password = document.getElementById('password').value;
 let cpassword = document.getElementById('cpassword').value;
 if(password.length<8 && cpassword.length<8 || password!==cpassword){
 alert("password lenght should to greater than 8 digit or password and conform passwod should be same");
 
+}else{
+alert("submit Successful");
 }
+})

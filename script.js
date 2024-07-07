55260224bf62f2c62c50fa1e44e463eff6c68f19
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-07 13:41:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+let password = document.getElementById('password').value;
+let cpassword = document.getElementById('cpassword').value;
+if(password.length<8 && cpassword.length<8 || password!==cpassword){
+alert("password lenght should to greater than 8 digit or password and conform passwod should be same");
+
+}
